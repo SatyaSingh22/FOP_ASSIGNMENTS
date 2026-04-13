48c5eb5d4709b6bb666d155fbd9cf932b1e18400
Satya Kumari Singh <satyasingh1622@gmail.com>
2026-04-13 23:11:58 +0530

    Add program to reverse digits of a number

---
+++ ASSIGNMENT13/program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, remainder;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Reversed digits: ");
+
+    while(num != 0) {
+        remainder = num % 10;  
+        printf("%d", remainder); 
+        num = num / 10;
+    }
+
+    return 0;
+}
